d8fcaba56085ffeda4efc185daaafb3cb36df806
MandaHariniaina <andriamitantsoamanda@gmail.com>
2024-02-24 09:38:55 +0300

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,8 @@ export const environment = {
   client_id: 'a2934fa2-6f7e-4ac9-8210-681814ac86c4',
   favorisClient: 'pages/favoris',
 
-  api_host:'http://localhost:3000',
+  // api_host:'https://salon-beaute-backend.onrender.com',
+  api_host:'http://localhost:8080',
 
   generate_token_uri: '/api/user/token',
   refresh_token_uri:'/api/auth/refreshtoken',
@@ -25,6 +26,12 @@ export const environment = {
   create_service_uri:"/api/service/manager/create",
   update_service_uri:"/api/service/manager/updateService",
   delete_service_uri:"/api/service/manager/deleteService",
-  create_rendezVous_uri:"/api/rendezVous",
-  add_upload_abmform_uri:""
+  add_upload_abmform_uri:"",
+  /* Rendez-vous */
+  getRendezVous: "/api/rendezVous/",
+  deleteRendezVous: "/api/rendezVous/",
+  createRendezVous: "/api/rendezVous/",
+  updateRendezVous: "/api/rendezVous/",
+  createPrestationFromRendezVous: "/api/rendezVous/create-prestation",
+  /* ----------- */
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,8 @@ export const environment = {
   auth_strategy_name: 'jwt',
   favorisClient: 'pages/favoris',
  
-  api_host:'http://localhost:3000',
+  // api_host:'https://salon-beaute-backend.onrender.com',
+  api_host:'http://localhost:8080',
  
   register_uri:'/api/auth/signup',
   generate_token_uri: '/api/user/token',
@@ -29,6 +30,12 @@ export const environment = {
   create_service_uri:"/api/service/manager/create",
   update_service_uri:"/api/service/manager/updateService",
   delete_service_uri:"/api/service/manager/deleteService",
-  create_rendezVous_uri:"/api/rendezVous",
-  add_upload_abmform_uri:""
+  add_upload_abmform_uri:"",
+  /* Rendez-vous */
+  getRendezVous: "/api/rendezVous/",
+  deleteRendezVous: "/api/rendezVous/",
+  createRendezVous: "/api/rendezVous/",
+  updateRendezVous: "/api/rendezVous/",
+  createPrestationFromRendezVous: "/api/rendezVous/create-prestation",
+  /* ----------- */
 };
